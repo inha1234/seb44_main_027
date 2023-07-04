89f30a77f8c739412a6549e26100e235a469938c
JKY4860 <124881646+JKY4860@users.noreply.github.com>
2023-07-04 13:49:24 +0900

    feat: Add post entity

---
+++ server/mainProject/src/main/java/com/codestates/mainProject/posts/entity/Post.java
@@ -0,0 +1,36 @@
+package com.codestates.mainProject.posts.entity;
+
+import com.codestates.mainProject.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //
+    private long postId;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private String category;
+//    @OneToMany
+//    private Member member;
+//    @ManyToOne(mappedBy = "post", cascade = {CascadeType.ALL})
+//    private List<Comment> comments = new ArrayList<>();
+//
+//    public void addComment(Comment comment){
+//        this.comments.add(comment);
+//    }
+
+}
