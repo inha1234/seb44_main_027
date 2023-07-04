4b6956d8f01079c9b76334c25f6ecd42ab4909ba
thewronghand <penfreak77@gmail.com>
2023-07-04 11:42:37 +0900

    feat: Route 설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Provider } from "react-redux"
 import store from "./redux/store"
+import Main from "./Pages/Main"
+import Login from "./Pages/Login"
 function App() {
 
   return (
     <>
     <Provider store={store}>
       <Router>
-        <Switch>
-          <Route/>
-        </Switch>
+        <Routes>
+        <Route exact path="/" element={<Main/>} />
+        <Route path="/login" element={<Login/>} />
+        </Routes>
       </Router>
     </Provider>
     </>
   )
 }
 
-export default App
+export default App;
