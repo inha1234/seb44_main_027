9ba72e8ab0d3b3d1d06386511c5b9d74a423f27d
thewronghand <penfreak77@gmail.com>
2023-07-06 01:07:38 +0900

    style: 스타일 파일 분리

--- client/src/Components/ImageCarousel.jsx
+++ client/src/Components/ImageCarousel.jsx
@@ -1,59 +1,6 @@
 import React from 'react';
-import Slider from "react-slick";
-import styled from 'styled-components';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import { StyledDots,SliderWrapper, StyledSlider } from './ImageCarousel.style';
 
-const SliderWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  position: relative;
-`;
-
-const StyledSlider = styled(Slider)`
-  width: 100%;
-  height: 100%;
-
-  .slick-slide div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh; 
-    width: 50vw; 
-  }
-
-  .slick-slide img {
-    width: auto;
-    height: auto;
-    max-height: 100vh; 
-    max-width: 50vw; 
-    object-fit: contain; 
-  }
-`;
-
-const StyledDots = styled.ul`
-  position: absolute;
-  bottom: 40px;
-  list-style: none;
-  display: flex;
-  justify-content: center;
-  padding: 0;
-  
-  li {
-    margin: 0 3px;
-    
-    button:before {
-      font-size: 15px;
-      color: #fff;
-      opacity: 0.75;
-    }
-    
-    &.slick-active button:before {
-      color: #fff;
-      opacity: 1;
-    }
-  }
-`;
 
 export default function ImageCarousel() {
     const settings = {
