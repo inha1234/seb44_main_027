95952e441cf4bb91f3b24dc8dbea7cfa53eaae6b
heowan <101536766+heowan@users.noreply.github.com>
2023-07-18 18:21:34 +0900

    feat: 메인화면 컴포넌트 구현

---
+++ client/src/Components/MainBoard.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { BoardBox, Title, CardList, Btn } from './MainBoard.style.js';
+import MainCardItem from './MainCardItem.jsx';
+import Loding from './Loding.jsx';
+import useInfiniteScroll from '../utils/hooks/useInfiniteScroll.js';
+
+function MainBoard() {
+  const MESSAGE = {
+    TITLE_MAIN: '오늘은 무슨 활동을 하셨나요?',
+    BTN_MAIN: '만들기',
+  };
+  // const loginId = sessionStorage.getItem('memberId');
+  const loginId = 2;
+  // get 요청 url
+  const url = `${import.meta.env.VITE_API_URL}/posts/followingPosts`;
+
+  // 무한스크롤 API
+  const [data, setData] = useState([]); // getData 함수를 통해 받아온 데이터의 상태 (Carditem에 전달 될 데이터)
+  const [page, setPage] = useState(0); // page 상태에 따라 api 요청에 lastPostId params를 추가 (page가 1일 경우 lastPostId 쿼리 없이 데이터 요청, 1일 아닐 경우 lastPostId 쿼리를 추가하여 데이터 요청)
+  const [ref, inView, getData, isLoadEnd] = useInfiniteScroll({
+    url,
+    category: 'main',
+    data,
+    setData,
+    page,
+    memberId: loginId,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      getData();
+      setPage(page + 1);
+    }
+  }, [inView]);
+
+  return (
+    <BoardBox>
+      <Title>
+        <h1>{MESSAGE.TITLE_MAIN}</h1>
+        <Btn to="/create">{MESSAGE.BTN_MAIN}</Btn>
+      </Title>
+      <CardList>
+        {data && data.length !== 0 ? (
+          data.map((item) => <MainCardItem key={item.postId} item={item} />)
+        ) : (
+          <div>친구가 없어요.</div>
+        )}
+      </CardList>
+      {isLoadEnd ? undefined : (
+        <div ref={ref}>
+          <Loding />
+        </div>
+      )}
+    </BoardBox>
+  );
+}
+
+export default MainBoard;
--- client/src/Pages/Main.jsx
+++ client/src/Pages/Main.jsx
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom';
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import MainBoard from '../Components/MainBoard';
+import { MainContainer } from './Main.style';
+import Nav from '../Components/Nav';
+import MyProfile from '../Components/MyProfile';
 function Main() {
   return (
     <>
-      main
-      <Link to="login">toLogin</Link>
-      <Link to="create">만들기</Link>
-      <Link to="workout">식단</Link>
-      <Link to="diet">운동</Link>
-      <Link to="profile/1234">유저프로필</Link>
+      <Nav />
+      <MainContainer>
+        <MainBoard />
+        <MyProfile />
+      </MainContainer>
     </>
   );
 }
