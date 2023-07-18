87955aa4d0f064d840bfcd8f0d99f3a0cc904dfd
thewronghand <penfreak77@gmail.com>
2023-07-18 11:17:37 +0900

    feat: 로그인 상태에서 로그인/회원가입 접근 차단

--- client/src/utils/PrivateRoute.jsx
+++ client/src/utils/PrivateRoute.jsx
@@ -3,12 +3,20 @@ import AuthRedirect from '../Components/AuthRedirect';
 
 function PrivateRoute({ children, path }) {
   let location = useLocation();
+  const isLoggedIn = Boolean(sessionStorage.getItem('authToken'));
 
-  if (!sessionStorage.getItem('authToken')) {
+  // 로그인이 안 된 상태에서 로그인이 필요한 페이지에 접근하는 경우
+  if (!isLoggedIn) {
     if (path !== '/') {
       return <AuthRedirect />;
+    } else {
+      return <Navigate to="/login" state={{ from: location }} />;
     }
-    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  // 로그인이 된 상태에서 로그인 또는 회원가입 페이지에 접근하는 경우
+  if (isLoggedIn && (path === '/login' || path === '/signup')) {
+    return <Navigate to="/" />;
   }
 
   return children;
