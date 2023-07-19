2048a8ad31866b18b8d5333dc2d842f64af2698a
heowan <101536766+heowan@users.noreply.github.com>
2023-07-20 02:47:15 +0900

    design: nav 디자인 수정, color.js 파일 생성

---
+++ client/src/color.js
@@ -0,0 +1,19 @@
+export const Grayscale = {
+  White: '#ffffff',
+  20: '#EDEDED',
+  40: '#BFBFBF',
+  60: '#808080',
+  80: '#262626',
+};
+
+export const Red = {
+  10: '#DA1E28',
+  20: '#BA1B23',
+};
+
+export const Color = {
+  Primary: '#12BFB5',
+  PrimaryRGB: '18,191,181',
+  Secondary: '#3029F2',
+  Tertiary: '#3658BF',
+};
