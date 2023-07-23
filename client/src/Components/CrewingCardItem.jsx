d23d043d1192d945932f807bfe8ae3dc6cc0bc63
heowan <101536766+heowan@users.noreply.github.com>
2023-07-24 02:21:41 +0900

    design: 크루잉 게시글 컴포넌트 디자인 수정

--- client/src/Components/CrewingCardItem.jsx
+++ client/src/Components/CrewingCardItem.jsx
@@ -2,10 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Card } from './CrewingCardItem.style';
 import PostDetailModal from './PostDetailModal';
 import formatTime from '../utils/formatTime';
-import { useSelector } from 'react-redux';
-
+import koTime from '../utils/koTime';
+import { getDDay } from '../utils/getDday';
 function CrewingCardItem({ item }) {
   const [isModal, setIsModal] = useState(false);
+  const isUnlimited = item.maxPeople === 999;
+  const applyStatus = item.completed
+    ? { className: 'end', text: '모집마감' }
+    : { className: 'apply', text: `모집중 D-${getDDay(item.deadLine)}` };
+
+  const inputActivityDate = koTime(item.activityDate);
+  const inputDeadLine = koTime(item.deadLine);
+  const regex = /^(\d{4}-\d{2}-\d{2}) (오전|오후) (\d{2}:\d{2})$/;
+  const activityDate = inputActivityDate.match(regex);
+  const deadLine = inputDeadLine.match(regex);
 
   const hendleClick = () => {
     setIsModal(!isModal);
@@ -27,7 +37,29 @@ function CrewingCardItem({ item }) {
             </Card.AuthorCreateAt>
           </Card.Author>
           <Card.Title>{item.title}</Card.Title>
+          <Card.Content>{item.content}</Card.Content>
+          <Card.Status className={applyStatus.className}>
+            {applyStatus.text}
+          </Card.Status>
         </Card.Info>
+        <Card.RecruitmentInfo className="recruitmentInfo">
+          <Card.ActivityDate>
+            <Card.Label>활동날짜</Card.Label>
+            <Card.Text>{activityDate[1]}</Card.Text>
+            <Card.Text>{`${activityDate[2]} ${activityDate[3]}`}</Card.Text>
+          </Card.ActivityDate>
+          <Card.DeadLine>
+            <Card.Label>모집마감</Card.Label>
+            <Card.Text>{deadLine[1]}</Card.Text>
+            <Card.Text>{`${deadLine[2]} ${deadLine[3]}`}</Card.Text>
+          </Card.DeadLine>
+          <Card.Personnel>
+            <Card.Label>참여인원</Card.Label>
+            <Card.Text>{`${item.currentPeople} / ${
+              isUnlimited ? '무제한' : item.maxPeople
+            }`}</Card.Text>
+          </Card.Personnel>
+        </Card.RecruitmentInfo>
       </Card.Container>
       {isModal ? (
         <PostDetailModal
--- client/src/Components/PostDetailModal.style.js
+++ client/src/Components/PostDetailModal.style.js
@@ -6,6 +6,7 @@ export const Modal = styled.section`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 10;
   width: 100%;
   height: 100%;
   padding: 0 100px;
